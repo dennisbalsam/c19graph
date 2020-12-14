bca2045dc290f1e4d0da2067d075b3981c75374d
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-12-13 23:57:58 -0500

    tracing graph on initial draw

--- graph_operations.py
+++ graph_operations.py
@@ -9,7 +9,13 @@ matplotlib.use('Agg')
 # define initial graph
 graph = nx.Graph()
 
-# get data from postgres
+# trace path of spread
+def tracePath(node, depth):
+    if depth == 0: return
+    
+    for i in graph.neighbors(node):
+        graph.add_edge(node, i, color='r')
+        tracePath(i, depth-1)
 
 # get initial data from database
 def initialGraph():
@@ -29,21 +35,30 @@ def initialGraph():
     cursor.execute('select * from Contact;')
     contact = cursor.fetchall()
 
+    # connect all people who have made contact
+    for row in contact:
+        graph.add_edge(row[0],row[1],color='k')
 
     # add all people
     for row in people:
-        graph.add_node(row[0], name=row[1], age=row[2], infected=row[3])
-
-    # connect all people who have made contact
-    for row in contact:
-        graph.add_edge(row[0],row[1])
+        if row[3] == True: # infected node
+            graph.add_node(row[0], name=row[1], age=row[2], infected=row[3], color='r')
+            tracePath(row[0],2)
+        else:
+            graph.add_node(row[0], name=row[1], age=row[2], infected=row[3], color='teal')
 
 
     draw()
 
+
+
 # draw graph
 def draw():
-    nx.draw_networkx(graph,font_color='black')
+    edge_colors = list(nx.get_edge_attributes(graph,'color').values())
+    node_colors = list(nx.get_node_attributes(graph,'color').values())
+
+    # nx.draw_networkx(graph,font_color='black')
+    nx.draw_networkx(graph, node_color=node_colors,edge_color=edge_colors, font_color='white', pos=nx.circular_layout(graph), width=2)
     plt.savefig('./static/img/graph.png') # save graph image
     plt.close()
 
