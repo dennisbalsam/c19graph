0d361a00cf9216571a935d3cc3dd4e6b8e79e6bb
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-12-14 01:05:40 -0500

    edge adding and initial infection toggle

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import json
 import random
 import os
 import subprocess
-from graph_operations import graph, initialGraph
+from graph_operations import graph, initialGraph, tracePath
 from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
-
-
-
 
 
 def updateAndDraw():
@@ -16,19 +13,34 @@ def updateAndDraw():
 @app.route("/")
 def index():
     initialGraph()
-    return render_template('index.html',filepath = f'static/img/graph.png') 
-
-@app.route("/addnode")
-def addnode():
-    graph.add_node(int(request.args.get('label')))
-    updateAndDraw()
-    return redirect(url_for('index'))
+    return render_template('index.html',
+        filepath = f'static/img/graph.png',
+        nodes = graph.nodes()) 
 
 @app.route("/addedge")
 def addedge():
     graph.add_edge(int(request.args.get('label1')),int(request.args.get('label2')),color='k')
     updateAndDraw()
     return redirect(url_for('index'))
+
+@app.route("/toggleinfection")
+def toggleinfection():
+
+    node = int(request.args.get('label'))
+    graph.nodes[node]['infected'] = not graph.nodes[node]['infected']
+
+    if graph.nodes[node]['infected'] == True :
+        graph.nodes[node]['color'] = 'r'
+        tracePath(node,2)
+    else:
+        graph.nodes[node]['color'] = 'teal'
+
+    print(graph.nodes[node]['infected'])
+    updateAndDraw()
+    return render_template('index.html',
+        filepath = f'static/img/graph.png',
+        nodes = graph.nodes()) 
+    # return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
